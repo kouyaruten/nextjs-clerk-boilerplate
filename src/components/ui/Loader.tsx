e394b7c573ef834a8df22894b524d0ead766cdb3
kouyaruten <williamjinq@outlook.com>
2024-11-03 17:52:21 +0800

    fixed loader issue

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,16 +1,19 @@
-'use client';
+"use client";
 
 export default function Loader() {
   return (
     <div
       className="w-[40px] p-1 aspect-square rounded-full bg-gray-200 dark:bg-gray-800 animate-spin"
-      style={{
-        '--_m': 'conic-gradient(#0000 10%,#000), linear-gradient(#000 0 0) content-box',
-        WebkitMask: 'var(--_m)',
-        mask: 'var(--_m)',
-        WebkitMaskComposite: 'source-out',
-        maskComposite: 'subtract',
-      }}
+      style={
+        {
+          ["--_m" as string]:
+            "conic-gradient(#0000 10%,#000), linear-gradient(#000 0 0) content-box",
+          WebkitMask: "var(--_m)",
+          mask: "var(--_m)",
+          WebkitMaskComposite: "source-out",
+          maskComposite: "subtract",
+        } as React.CSSProperties
+      }
     />
   );
 }
